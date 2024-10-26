c7423f0fdc5002675b5671e0e9257b9478bcd9b9
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-10-26 19:42:36 +0700

    feat: implement des client server with socket

--- client.py
+++ client.py
@@ -1,25 +1,29 @@
 import socket
+from desAlgo import DES
+
+algo = DES()
+key = "AMANAJAA"
+print(f"Key: {key}")
+key_bin = algo.ascii_to_bin(key)
+rkb, rk = algo.generate_keys(key_bin)
+rkb_rev = rkb[::-1]
+rk_rev = rk[::-1]
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 1234))
 
 while True:
-    client.send(input("B :").encode("utf-8"))
-    msg = client.recv(1024).decode("utf-8")
-    if msg.lower() == 'quit':
+    msg = input("B :")
+    msg_bin = algo.ascii_to_bin(msg)
+    encrypted_msg = algo.encrypt(msg_bin, rkb, rk)
+    # client.send(algo.bin2hex(encrypted_msg).encode("utf-8"))
+    client.send(encrypted_msg.encode("utf-8"))
+
+    encrypted_res = client.recv(1024).decode("utf-8")
+    if encrypted_res.lower() == 'quit':
         break
     else :
-        print("A :", msg)
+        decrypted_res = algo.bin_to_ascii(algo.decrypt(encrypted_res, rkb_rev, rk_rev))
+        print("A :", decrypted_res)
 
 client.close()
-
-# try:
-#     while True:
-#         message = input("message : ")
-#         if message.lower() == 'quit':
-#             break
-#         client.send(message.encode())
-#         response = client.recv(1024).decode()
-#         print("response:", response)
-# finally:
-#     client.close()
--- server.py
+++ server.py
@@ -1,5 +1,15 @@
 import socket
+from desAlgo import DES
 
+algo = DES()
+key = "AMANAJAA"
+print(f"Key: {key}")
+key_bin = algo.ascii_to_bin(key)
+rkb, rk = algo.generate_keys(key_bin)
+rkb_rev = rkb[::-1]
+rk_rev = rk[::-1]
+
+# --- BUAT SETUP KONEKSI
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('localhost', 1234))
 server.listen(5)
@@ -12,21 +22,14 @@ while True:
     if msg.lower() == 'quit':
         break
     else :
-        print("B :", msg)
+        # msg_bin = algo.ascii_to_bin(msg)
+        decrypted_msg = algo.bin_to_ascii(algo.decrypt(msg, rkb_rev, rk_rev))
+        print("B :", decrypted_msg)
     
-    client.send(input("A :").encode("utf-8"))
+    res = input("A :")
+    res_bin = algo.ascii_to_bin(res)
+    encrypted_res = algo.encrypt(res_bin, rkb, rk)
+    client.send(encrypted_res.encode("utf-8"))
 
 client.close()
 server.close()
-
-    # try:
-    #     while True:
-    #         data = client.recv(1024)
-    #         if not data:
-    #             break
-    #         print("from client:", data.decode())
-    #         client.send("msg received".encode())
-    # finally:
-    #     client.close()
-    #     print(f"connection closed from {addr}")
-    #     break
